8097d60b67d2e0ce6146b97dd03179f0ca2e398b
qianqiang.liu <qianqiang.liu@amlogic.com>
2022-06-22 23:27:53 +0800

    remeber last position

--- lua/options.lua
+++ lua/options.lua
@@ -5,3 +5,5 @@ vim.o.number = true
 vim.o.numberwidth = 1
 vim.o.relativenumber = true
 vim.o.tags = './.tags;,.tags'
+
+vim.api.nvim_exec([[ autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif ]], false)
